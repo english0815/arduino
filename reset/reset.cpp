4d78f84fb34062fe1ada0525657e79277e1477bb
Allen Hsu <english0815@gmail.com>
2021-08-25 16:32:36 +0800

    [arduino : reset] Config reset time function.
    Description:
    ------------
    1. config reset time.
    2. Add help messages.
        
    signed-off-by: Allen Hsu <english0815@gmail.com>

--- reset/reset.cpp
+++ reset/reset.cpp
@@ -1,67 +1,90 @@
-#include <Arduino.h>
+/* Pin definition */
 int RELAYpin = 7;
 
-void setup() {
-	Serial.begin(9600);
-	while (!Serial) {
-		; // wait for serial port to connect. Needed for native USB port only
-	}
-
-	pinMode(RELAYpin, OUTPUT);
-	pinMode(LED_BUILTIN, OUTPUT);
-	digitalWrite(RELAYpin, HIGH);
-	digitalWrite(LED_BUILTIN, HIGH);
-
-	Serial.println("");
-	Serial.println("> Input the string:");
-	Serial.println("  =================");
-	Serial.println("  on:    Power off");
-	Serial.println("  off:   Power on");
-	Serial.println("  reset: Reset");
-	Serial.println("");
-	Serial.print("> ");
-}
-
+/* Public variable */
 String s = "";
+int wait_time = 3;
+
+void help() 
+{
+  Serial.println("");
+  Serial.println("> Input the string:");
+  Serial.println("  =================");
+  Serial.println("  on:    Power off");
+  Serial.println("  off:   Power on");
+  Serial.println("  reset: Reset (default time: 3s)");
+  Serial.println("  config time <time>: config reset time (Unit: second)");
+  Serial.println("");
+  Serial.print("> ");
+} 
 
 void strcmd(String cmd)
 {
-	if (cmd == "off") {
-		Serial.println("Power off ...");
-		digitalWrite(RELAYpin, LOW);
-	} else if (cmd == "on") {
-		Serial.println("Power on ...");
-		digitalWrite(RELAYpin, HIGH);
-	} else if (cmd == "reset") {
-		Serial.println("Wait 3s, and reset ...");
-		digitalWrite(RELAYpin, LOW);
-		delay(3000);
-		digitalWrite(RELAYpin, HIGH);
-	} else {
-		Serial.println("No support ...");
-	}
-	
+  if (cmd == "off") {
+    Serial.println("Power off ...");
+    digitalWrite(RELAYpin, LOW);
+  } else if (cmd == "on") {
+    Serial.println("Power on ...");
+    digitalWrite(RELAYpin, HIGH);
+  } else if (cmd == "reset") {
+    Serial.print("Wait ");
+    Serial.print(wait_time);
+    Serial.println("s, and reset ...");
+    digitalWrite(RELAYpin, LOW);
+    delay(wait_time * 1000);
+    digitalWrite(RELAYpin, HIGH);
+  } else if (cmd == "help") {
+    help();
+  } else if (cmd.substring(0, 11) == "config time") { // config time s
+    int tmp_time = cmd.substring(11).toInt();
+    
+    if (tmp_time == 0) {
+      Serial.print("Invalid input: ");
+      Serial.println(tmp_time);
+      return;
+    }
+    
+    wait_time = tmp_time;
+    Serial.print("Config reset time to ");
+    Serial.println(wait_time);
+  } else {
+    Serial.println("No support ...");
+  }
+  
+}
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  pinMode(RELAYpin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(RELAYpin, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  help();
 }
 
 void loop() {
-	if (Serial.available() > 0) {
-		char c = Serial.read();
-		if (c != '\r') {
-			s += c;
-			Serial.print(c);
-			if (c == '\b') {
-				Serial.print(" ");
-				Serial.print(c);
-				s.remove(s.length()-2);
-			}
-		} else {
-			Serial.println("");
-			if (s != "") {
-				strcmd(s);
-			}
-			s = "";
-			Serial.println("");
-			Serial.print("> ");
-		}
-	}
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c != '\r') {
+      s += c;
+      Serial.print(c);
+      if (c == '\b') {
+        Serial.print(" ");
+        Serial.print(c);
+        s.remove(s.length()-2);
+      }
+    } else {
+      Serial.println("");
+      if (s != "") {
+        strcmd(s);
+      }
+      s = "";
+      Serial.println("");
+      Serial.print("> ");
+    }
+  }
 }
